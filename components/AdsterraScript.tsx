821b6cce18047659a18d30c349ab3c873edd79ef
Harsha <118063362+harshascode@users.noreply.github.com>
2024-08-07 01:27:29 +0530

    Update AdsterraScript.tsx

--- components/AdsterraScript.tsx
+++ components/AdsterraScript.tsx
@@ -12,3 +12,5 @@ const AdsterraScript = () => (
 );
 
 export default AdsterraScript;
+
+//<script type='text/javascript' src='//pl23959426.highratecpm.com/6b/01/b8/6b01b8707d5332bc63b07962c02ba5cc.js'></script>
