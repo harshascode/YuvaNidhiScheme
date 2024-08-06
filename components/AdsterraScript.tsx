d0baaaa0809e7c8212b241056817dbad5684d973
Harsha <118063362+harshascode@users.noreply.github.com>
2024-08-07 01:23:19 +0530

    Create AdsterraScript.tsx

---
+++ components/AdsterraScript.tsx
@@ -0,0 +1,14 @@
+// components/AdsterraScript.tsx
+import Script from 'next/script';
+
+const AdsterraScript = () => (
+  <>
+    <Script
+      id="adsterra-script"
+      src="//pl23959426.highratecpm.com/6b/01/b8/6b01b8707d5332bc63b07962c02ba5cc.js"
+      strategy="afterInteractive" // or "afterInteractive" if you need it to load earlier
+    />
+  </>
+);
+
+export default AdsterraScript;
